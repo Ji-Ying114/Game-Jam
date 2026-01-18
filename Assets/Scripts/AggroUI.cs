12e1f8251dfdfa8e439b796d87d4bfe9759c7233
Ji-Ying114 <1991244803@qq.com>
2026-01-18 14:32:11 +0800

    新增Aggro显示
    新增Aggro显示
    - 新增Aggro UI预制件；
    - 新增AggroUI脚本；
    - 新增UpdateAggro(float aggroValue)方法；
    - 新增ShowUI()方法；
    - 新增HideUI()方法；

---
+++ Assets/Scripts/AggroUI.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AggroUI : MonoBehaviour
+{   
+    [SerializeField] SpriteRenderer ui;
+    [SerializeField] Sprite[] aggroSprites; //存储不同aggro等级的图片
+//-----------------------------------------------------------------------------------
+     private float aggroValue;
+//-----------------------------------------------------------------------------------
+    void Start()
+    {
+        ui.enabled = false;
+    }
+//-----------------------------------------------------------------------------------
+    //根据aggro值更新UI显示的图片
+    void Update(){
+        if (aggroValue >= 0 && aggroValue < 20)
+        {
+            ui.sprite = aggroSprites[0];
+        }
+        else if (aggroValue >= 20 && aggroValue < 40)
+        {
+            ui.sprite = aggroSprites[1];
+        }
+        else if (aggroValue >= 40 && aggroValue < 60)
+        {
+            ui.sprite = aggroSprites[2];
+        }
+        else if (aggroValue >= 60 && aggroValue < 80)
+        {
+            ui.sprite = aggroSprites[3];
+        }
+        else if (aggroValue >= 80 && aggroValue <= 100)
+        {
+            ui.sprite = aggroSprites[4];
+        }
+    }
+//-----------------------------------------------------------------------------------
+    //更新aggro值
+    public void UpdateAggro(float aggroValue)
+    {
+        this.aggroValue = aggroValue;
+    }
+//-----------------------------------------------------------------------------------
+    //显示/隐藏UI
+    public void ShowUI()
+    {
+        ui.enabled = true;
+    }
+    public void HideUI()
+    {
+        ui.enabled = false;
+    }
+}
